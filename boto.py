2fd5379c4446d0da664f0856a4374558e3ffcc3c
itc_user <milacukierman@gmail.com>
2016-07-28 16:50:16 +0300

    adding some code

--- boto.py
+++ boto.py
@@ -5,15 +5,54 @@ from bottle import route, run, template, static_file, request
 import json
 
 
+list_boto = {
+    "camila":{"msg": "Hello, lest talk..,","animation":"dancing"},
+    "felling": {"msg":"I am fine","animation":"excited"},
+    "hobby": {"msg":"i like to do some code....","animation":"heartbroke"},
+    "movie":{"msg":"the beauty and the beast","animation":"inlove"},
+    "sport":{"msg":"i dont like sport, i like dancing","animation":"dancing"},
+    "music":{"msg":"mizhrari","animation":"ok"},
+    "drink":{"msg":"cola zero, is the best","animation":"excited"},
+    "food":{"msg":"sushi, do u want to invite me to eat some sushi tonigth?","animation":"inlove"},
+    "love": {"msg": "of course i love u.... do you love ", "animation": "inlove"}
+}
+
+
 @route('/', method='GET')
 def index():
     return template("chatbot.html")
 
 
+def handleSimpleAsnwers(msg):
+    if msg.lower() == "yes":
+        return {"msg":"I love you !thanks for beeing so nice","animation":"inlove"}
+    if msg.lower() == "no":
+        return {"msg": "dont ne rude....", "animation": "no"}
+    return None
+
+def handleWanted(msg):
+    if msg.startswith("i wish"):
+        return {"msg":"im sorry, i cant make your wish come true","animation":"afraid"}
+    return None
+
+def handleBadWords(msg):
+    if msg.startswith("fuck you", "i dont like u", "shit"):
+        return {"msg":"fuck u","animation":"takeoff"}
+    return None
+
 @route("/chat", method='POST')
 def chat():
     user_message = request.POST.get('msg')
-    return json.dumps({"animation": "inlove", "msg": user_message})
+    result = handleSimpleAsnwers(user_message)
+    result = handleWanted(user_message)
+    result = handleBadWords(user_message)
+    if not result:
+        for word in user_message.split(" "):
+            if word in list_boto:
+                return json.dumps(list_boto[word])
+
+        return json.dumps({"animation": "giggling", "msg": "I don't understand,sorry"})
+    return json.dumps(result)
 
 
 @route("/test", method='POST')
